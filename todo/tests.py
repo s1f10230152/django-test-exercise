3730c831fed275dbbee885c6d0fdfbb6048612c9
1F10230152 <s1f102301527@iniad.org>
2024-07-04 13:50:54 +0900

    コードを整形

--- todo/tests.py
+++ todo/tests.py
@@ -5,10 +5,14 @@ from todo.models import Task
 
 # Create your tests here.
 class SampleTestCase(TestCase):
+
+
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
 
 class TaskModelTestCase(TestCase):
+
+
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         task = Task(title='task1', due_at=due)
@@ -52,6 +56,8 @@ class TaskModelTestCase(TestCase):
         self.assertFalse(task.is_overdue(current))
     
 class TodoViewTestCase(TestCase):
+
+    
     def test_index_get(self):
         client = Client()
         response = client.get('/')
@@ -79,8 +85,8 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(response.context['tasks'][0],task2)
-        self.assertEqual(response.context['tasks'][1],task1)
+        self.assertEqual(response.context['tasks'][0], task2)
+        self.assertEqual(response.context['tasks'][1], task1)
 
     def test_index_get_order_due(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
@@ -92,5 +98,5 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(response.context['tasks'][0],task1)
-        self.assertEqual(response.context['tasks'][1],task2)
+        self.assertEqual(response.context['tasks'][0], task1)
+        self.assertEqual(response.context['tasks'][1], task2)
--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,8 @@ from todo.models import Task
 
 # Create your views here.
 def index(request):
+
+    
     if request.method == 'POST':
         task = Task(title=request.POST['title'],
                     due_at=make_aware(parse_datetime(request.POST['due_at'])))
@@ -16,7 +18,7 @@ def index(request):
         tasks = Task.objects.order_by('-posted_at')
 
     context = {
-        'tasks' : tasks
+        'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
         
